2f45ba82ba7a23327f2a7f016c0170d4b480c4b0
OlgaMekka <138327607+OlgaMekka@users.noreply.github.com>
2023-08-08 15:07:37 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,10 @@
+# Файл «configuration.py» необходим для хранения URL и путей запроса
+
+# 1. URL «Яндекс Самокат»
+URL_SERVICE = "https://c395ad60-e962-439d-a1f0-33d806f1ffe0.serverhub.praktikum-services.ru"
+
+# 2. Путь до GET-запроса на получение информации о заказе
+GET_INFO_ORDER = "/api/v1/orders/track"
+
+# 3. Путь до POST-запроса на создание нового заказа
+POST_CREATE_NEW_ORDER = "/api/v1/orders"
